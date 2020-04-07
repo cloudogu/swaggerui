a9381593eba7dd21ffa139428c8ad7f879fa6e02
Christian Beyer <christian.beyer@cloudogu.com>
2020-04-07 08:24:46 +0200

    add states to startup script

--- resources/startup.sh
+++ resources/startup.sh
@@ -5,9 +5,12 @@ set -o pipefail
 
 NGINX_ROOT=/var/www/html
 INDEX_FILE=$NGINX_ROOT/index.html
+doguctl state "starting"
 
 # remove swagger.json from index.html
 sed -i "s|https://petstore.swagger.io/v2/swagger.json||g" $INDEX_FILE
+
+doguctl state "ready"
 
 # Start nginx
 echo "[nginx] starting nginx service..."
